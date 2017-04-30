a9679613867f39c59ea75295ca00e1ceda833d9d
zspengyou <zspengyou@gmail.com>
2017-04-30 14:36:45 -0400

    add new file

---
+++ learning2/src/lintCode/SlidingWindowMaximum.java
@@ -0,0 +1,14 @@
+package lintCode;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+
+public class SlidingWindowMaximum {
+
+	public static void main(String[] args) {
+		Queue<Integer> queue = new PriorityQueue<Integer>();
+
+	}
+
+}
