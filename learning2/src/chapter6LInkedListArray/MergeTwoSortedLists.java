be3e001b7acc23c72c7ab614cc2885abb3ee9ebb
zspengyou <zspengyou@gmail.com>
2017-05-04 23:36:33 -0400

    merge two sorted list

---
+++ learning2/src/chapter6LInkedListArray/MergeTwoSortedLists.java
@@ -0,0 +1,29 @@
+package chapter6LInkedListArray;
+
+import lintCode.ListNode;
+
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        ListNode dummy = new ListNode(0);
+        ListNode runner = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                runner.next = l1;
+                l1 = l1.next;
+            } else {
+                runner.next = l2;
+                l2 = l2.next;
+            }
+            runner = runner.next;
+        }
+        if (l1 != null)
+            runner.next = l1;
+        if (l2 != null)
+            runner.next = l2;
+        return dummy.next;
+    }
+}
